82c7247cd6a0bec4e47cad92d5ed6f961a10e75b
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-10-12 14:18:20 -0600

    Seperate data loader from code.

--- train_test/train_test_TSVIT_zueri_crop.py
+++ train_test/train_test_TSVIT_zueri_crop.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset, random_split
-from TSViT import TSViT
+from models.TSViT import TSViT
+from data_loader.numpy_dataloader_zuericop import CustomDataset
 import numpy as np
 import os
 import csv
@@ -15,39 +16,6 @@ def export_results_to_csv(results, output_csv_path):
         for result in results:
             writer.writerow(result)
     print(f"Results saved to {output_csv_path}")
-
-
-
-# Custom Dataset Clas
-class CustomDataset(Dataset):
-    def __init__(self, dataset_dir):
-        self.dataset_dir = dataset_dir
-        # List all folders inside the dataset directory
-        self.data_paths = [
-            os.path.join(self.dataset_dir, d)
-            for d in os.listdir(self.dataset_dir)
-            if os.path.isdir(os.path.join(self.dataset_dir, d))
-        ]
-
-    def __len__(self):
-        return len(self.data_paths)
-
-    def __getitem__(self, idx):
-        folder_path = self.data_paths[idx]
-        # Load image data from 'data.npy'
-        image_path = os.path.join(folder_path, "data.npy")
-        image = np.load(image_path)
-
-        # Load label data from 'gt.npy'
-        label_path = os.path.join(folder_path, "gt.npy")
-        label = np.load(label_path)
-        label = np.squeeze(label)
-
-        # Convert to PyTorch tensors
-        image_tensor = torch.tensor(image, dtype=torch.float32)
-        label_tensor = torch.tensor(label, dtype=torch.long)
-
-        return image_tensor, label_tensor
 
 
 # Simplified Training Loop
