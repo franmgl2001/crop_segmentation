695fc193e9f65e2d2a3714b0e8b30b417cc83fc6
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-10-17 23:19:40 -0600

    Fix paths

--- train_test/data_loader/pastis/cheap_transforms_pastis.py
+++ train_test/data_loader/pastis/cheap_transforms_pastis.py
@@ -6,7 +6,7 @@ import json
 from data_loader.pastis.dates_management import get_features_by_id, number_dates_by_difference
 
 
-with open("metadata.geojson") as f:
+with open("dataloader/pastis/metadata.geojson") as f:
     metadata = json.load(f)
 
 class ToTensor(object):
