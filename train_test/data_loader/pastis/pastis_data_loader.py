c7a380c29c7f5a0eb0d19b81a913f90565a769e3
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-10-17 23:00:18 -0600

    Add train test pastis

---
+++ train_test/data_loader/pastis/pastis_data_loader.py
@@ -0,0 +1,35 @@
+import json
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from cheap_transforms_pastis import process_image
+
+
+class CropDataset(Dataset):
+    def __init__(self, image_ids, metadata, seq_len=37):
+        self.image_ids = image_ids
+        self.metadata = metadata
+        self.seq_len = seq_len
+
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        return process_image(image_id, self.seq_len, "../../datasets/PASTIS/PASTIS-R/")
+
+def load_dataset(json_path, metadata, batch_size=32, shuffle=True, seq_len=37):
+    with open(json_path, 'r') as f:
+        data_split = json.load(f)
+    
+    train_dataset = CropDataset(data_split["train"], metadata, seq_len=seq_len)
+    test_dataset = CropDataset(data_split["test"], metadata, seq_len=seq_len)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+# Example usage
+# metadata = load_your_metadata_function()  # Load your metadata here
+train_loader, test_loader = load_dataset('train_test_split.json', metadata={})
