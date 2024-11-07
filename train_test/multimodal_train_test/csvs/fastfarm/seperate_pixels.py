9026f3d5ef8a9ca6cd83b2b86922d8ab1d7f6e4b
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-07 15:14:36 -0600

    Add train_test paths.

--- train_test/multimodal_train_test/csvs/fastfarm/seperate_pixels.py
+++ train_test/multimodal_train_test/csvs/fastfarm/seperate_pixels.py
@@ -7,12 +7,25 @@ data = pd.read_csv(file_path)
 
 # Filter out rows where all pixel values are zero
 # Assuming 'pixels' columns are all columns after the first one
-print(data.dtypes)
 filtered_data = data[(data['1'] != 0) | (data['2'] != 0)]
+
+
+filtered_data["path"] = filtered_data["field_id"].astype(str) + "_" + filtered_data["year"].astype(str) + ".pkl"
 
 # Perform an 80-20 train-test split
 train_data, test_data = train_test_split(filtered_data, test_size=0.2)
 
-# Save the train and test sets to separate files
-train_data.to_csv('train_data.csv', index=False)
-test_data.to_csv('test_data.csv', index=False)
+
+# Generate txt files of paths that are used in the CustomDataset class
+train_paths = train_data["path"].values
+test_paths = test_data["path"].values
+
+with open("train.txt", "w") as f:
+    for path in train_paths:
+        f.write(path + "\n")
+
+with open("test.txt", "w") as f:
+    for path in test_paths:
+        f.write(path + "\n")
+
+# Add column called 
