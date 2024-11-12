0a6c349a04f45e144b56a01ca31cd67f9ac740db
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-12 16:24:04 -0600

    Add seperate pixels.

--- train_test/multimodal_train_test/csvs/fastfarm/seperate_pixels.py
+++ train_test/multimodal_train_test/csvs/fastfarm/seperate_pixels.py
@@ -2,15 +2,23 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Load the CSV file
-file_path = 'pixel_counts.csv'
+file_path = "pixel_counts.csv"
 data = pd.read_csv(file_path)
 
 # Filter out rows where all pixel values are zero
-# Assuming 'pixels' columns are all columns after the first one
-filtered_data = data[(data['1'] != 0) | (data['2'] != 0)]
+# Filter columns with names from '1' to '2000'
+pixel_columns = [col for col in data.columns if col.isdigit() and 1 <= int(col) <= 2000]
+
+# Filter out rows where all pixel values are zero
+filtered_data = data[data[pixel_columns].sum(axis=1) > 0]
 
 
-filtered_data["path"] = filtered_data["field_id"].astype(str) + "_" + filtered_data["year"].astype(str) + ".pkl"
+filtered_data["path"] = (
+    filtered_data["field_id"].astype(str)
+    + "_"
+    + filtered_data["year"].astype(str)
+    + ".pkl"
+)
 
 # Perform an 80-20 train-test split
 train_data, test_data = train_test_split(filtered_data, test_size=0.2)
@@ -28,4 +36,4 @@ with open("test.txt", "w") as f:
     for path in test_paths:
         f.write(path + "\n")
 
-# Add column called 
+# Add column called
