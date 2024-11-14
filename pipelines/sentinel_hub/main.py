44c0daaed6b94192825313ff3656de2e33db90a3
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-14 15:10:17 -0600

    Add paralel processing

--- pipelines/sentinel_hub/main.py
+++ pipelines/sentinel_hub/main.py
@@ -1,17 +1,11 @@
 from functions.pipe_images import process_sentinel_images
 from functions.sentinel_hub_request import download_sentinel_images_in_range
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-from sentinelhub import SHConfig
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 csv = pd.read_csv("FASTFARM/fields_cim_1.csv")
 csv["year"] = csv["year"].astype(int)
 
-import pandas as pd
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-# Load your CSV file
-csv = pd.read_csv("your_file.csv")
 
 # Define your function to process each row
 def process_row(row):
@@ -35,34 +29,3 @@ with ThreadPoolExecutor(max_workers=5) as executor:
         future.result()
 
 
-
-def process_images_parallel(csv_file):
-    # Read the CSV file containing the points
-    csv = pd.read_csv(csv_file)
-
-    # Use ThreadPoolExecutor to parallelize the processing of images
-    with ThreadPoolExecutor(
-        max_workers=5
-    ) as executor:  # Adjust `max_workers` based on your system's capabilities
-        futures = [
-            executor.submit(
-                process_sentinel_images,
-                (row["point_lat"], row["point_long"]),
-                row["point_id"],
-                area_hectares=40,
-                start_date="2022-01-01",
-                end_date="2022-12-31",
-            )
-            for _, row in csv.iterrows()
-        ]
-
-        # Wait for all futures to complete
-        for future in futures:
-            try:
-                future.result()
-            except Exception as e:
-                print(f"Error processing image: {e}")
-
-
-# Example usage
-# process_images_parallel("Lucas_italy/Lucas_points_2022.csv")
