05ad5724a98e5df24c9e4988ac2adbc1286d8a2c
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-14 20:53:23 -0600

    Add type change

--- train_test/multimodal_train_test/transforms/fastfarm/transform_pickle.py
+++ train_test/multimodal_train_test/transforms/fastfarm/transform_pickle.py
@@ -190,11 +190,12 @@ def get_fielduse_count(field_id, year):
 
 
 def get_field_id_fielduses(field_id, year):
-    fielduses = pd.read_csv("../csvs/full_fielduses.csv")
+    fielduses = pd.read_csv("full_fielduses.csv")
+    fielduses["field_id"] = fielduses["field_id"].astype(str)
+    fielduses["year"] = fielduses["year"].astype(int, errors="ignore")
     fields_fielduses = fielduses[
         (fielduses["field_id"] == field_id) & (fielduses["year"] == year)
     ]
-    print(fields_fielduses)
     return fields_fielduses
 
 
