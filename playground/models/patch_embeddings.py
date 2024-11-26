6cd537ef0527031f3d51f7b5952941fca32372eb
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-26 13:52:23 -0600

    Add models

--- playground/models/patch_embeddings.py
+++ playground/models/patch_embeddings.py
@@ -1,19 +1,26 @@
 from torch import nn
 from einops import rearrange
 
+
 class StandardPatchEmbedding(nn.Module):
     def __init__(self, inner_dim, num_channels: list, patch_size):
         super().__init__()
         self.patch_size = patch_size
-        
-        patch_dim = sum(num_channels) * self.patch_size ** 2  
+
+        patch_dim = sum(num_channels) * self.patch_size**2
         self.linear_layer = nn.Linear(patch_dim, inner_dim)
-    
+
     def forward(self, x):
-        x = rearrange(x, 'b t c (h p1) (w p2) -> (b h w) t (p1 p2 c)', p1=self.patch_size, p2=self.patch_size)
+        x = rearrange(
+            x,
+            "b t c (h p1) (w p2) -> (b h w) t (p1 p2 c)",
+            p1=self.patch_size,
+            p2=self.patch_size,
+        )
         x = self.linear_layer(x)
         # B*N_H*N_W x T x d
         return x
+
 
 class MultiModalityEmbedding(nn.Module):
     def __init__(self, inner_dim, num_channels: list, patch_size):
@@ -21,23 +28,23 @@ class MultiModalityEmbedding(nn.Module):
         self.patch_size = patch_size
         self.num_channels = num_channels
         self.num_modalities = len(num_channels)
-        
+
         # one separate patch embedding for each modality
         self.patch_embed = nn.ModuleList(
             StandardPatchEmbedding(inner_dim, [i], patch_size)
             for i in self.num_channels
         )
-        
+
     def forward(self, x):
         # split by modality
         modality_input = []
 
-        channel_idx = self.num_channels # list of number of channels 
+        channel_idx = self.num_channels  # list of number of channels
         for i in range(self.num_modalities):
-            x1 = x[:,:, sum(channel_idx[:i]) : sum(channel_idx[:i+1])]
+            x1 = x[:, :, sum(channel_idx[:i]) : sum(channel_idx[: i + 1])]
             modality_input += [self.patch_embed[i](x1)]
-        
+
         # concatenate modality tokens in time dimension: one token time series for each modality
-        x = modality_input # 3 x B*H*W x T x d
-        
+        x = modality_input  # 3 x B*H*W x T x d
+
         return x
