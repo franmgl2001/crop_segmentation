4883e95f8a5606109fa826e76952313cfd3b005d
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-25 19:31:40 -0600

    Fix json

--- train_test/deep_sat_train_test/load_pastis.py
+++ train_test/deep_sat_train_test/load_pastis.py
@@ -1,25 +1,26 @@
 from models.TSViTcls import TSViTcls
 import torch
 
-model_config = {
-        'img_res': 24,
-        'patch_size': 3,
-        'patch_size_time': 1,
-        'patch_time': 4,
-        'num_classes': 20,
-        'max_seq_len': 16,
-        'dim': 128,
-        'temporal_depth': 10,
-        'spatial_depth': 4,
-        'depth': 4,
-        'heads': 3,
-        'pool': 'cls',
-        'num_channels': 14,
-        'dim_head': 64,
-        'dropout': 0.,
-        'emb_dropout': 0.,
-        'scale_dim': 4
-    }
+model_config ={
+    'img_res': 24,
+    'patch_size': 2,
+    'patch_size_time': 1,
+    'patch_time': 4,
+    'num_classes': 19,
+    'max_seq_len': 60,
+    'dim': 64,
+    'temporal_depth': 4,
+    'spatial_depth': 4,
+    'depth': 4,
+    'heads': 4,
+    'pool': 'cls',
+    'num_channels': 11,
+    'dim_head': 16,
+    'dropout': 0.,
+    'emb_dropout': 0.,
+    'scale_dim': 4
+}
+
 
 model = TSViTcls(model_config)
 
