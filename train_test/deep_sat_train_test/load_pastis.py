8bc10e4239163f127d14478f2ce92dc33bbd10e7
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-26 22:23:03 -0600

    Fix config

--- train_test/deep_sat_train_test/load_pastis.py
+++ train_test/deep_sat_train_test/load_pastis.py
@@ -1,31 +1,27 @@
 from models.TSViTdense import TSViT
 import torch
 
-model_config ={
-    'img_res': 24,
-    'patch_size': 2,
-    'patch_size_time': 1,
-    'patch_time': 4,
-    'num_classes': 19,
-    'max_seq_len': 60,
-    'dim': 64,
-    'temporal_depth': 4,
-    'spatial_depth': 4,
-    'depth': 4,
-    'heads': 4,
-    'pool': 'cls',
-    'num_channels': 11,
-    'dim_head': 16,
-    'dropout': 0.,
-    'emb_dropout': 0.,
-    'scale_dim': 4
+model_config = {
+    "img_res": 24,
+    "patch_size": 2,
+    "patch_size_time": 1,
+    "patch_time": 4,
+    "num_classes": 19,
+    "max_seq_len": 60,
+    "dim": 128,
+    "temporal_depth": 4,
+    "spatial_depth": 4,
+    "depth": 4,
+    "heads": 4,
+    "pool": "cls",
+    "num_channels": 11,
+    "dim_head": 32,
+    "dropout": 0.0,
+    "emb_dropout": 0.0,
+    "scale_dim": 4,
 }
 
 
 model = TSViT(model_config)
 
-model.load_state_dict(
-    torch.load("best.pth", map_location=torch.device("cpu"))
-)
-
-
+model.load_state_dict(torch.load("best.pth", map_location=torch.device("cpu")))
