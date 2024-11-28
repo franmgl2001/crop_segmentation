59f0f3d919fbfc28e48b7ce02ddb2bcc2ea01860
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2024-11-28 11:57:28 -0600

    Add permute

--- train_test/deep_sat_train_test/train_test.py
+++ train_test/deep_sat_train_test/train_test.py
@@ -11,6 +11,73 @@ test_csv_file = "pickle_paths.csv"
 root_dir = "./"
 epochs = 10
 
+import torch
+
+
+class MaskedCrossEntropyLoss(torch.nn.Module):
+    def __init__(self, mean=True):
+        """
+        mean: return mean loss vs per element loss
+        """
+        super(MaskedCrossEntropyLoss, self).__init__()
+        self.mean = mean
+
+    def forward(self, logits, ground_truth):
+        """
+        Args:
+            logits: (N,T,H,W,...,NumClasses)A Variable containing a FloatTensor of size
+                (batch, max_len, num_classes) which contains the
+                unnormalized probability for each class.
+            target: A Variable containing a LongTensor of size
+                (batch, max_len) which contains the index of the true
+                class for each corresponding step.
+            length: A Variable containing a LongTensor of size (batch,)
+                which contains the length of each data in a batch.
+        Returns:
+            loss: An average loss value masked by the length.
+        """
+        if type(ground_truth) == torch.Tensor:
+            target = ground_truth
+            mask = None
+        elif len(ground_truth) == 1:
+            target = ground_truth[0]
+            mask = None
+        elif len(ground_truth) == 2:
+            target, mask = ground_truth
+        else:
+            raise ValueError(
+                "ground_truth parameter for MaskedCrossEntropyLoss is either (target, mask) or (target)"
+            )
+
+        if mask is not None:
+
+            mask_flat = mask.reshape(-1, 1)  # (N*H*W x 1)
+            nclasses = logits.shape[-1]
+            logits_flat = logits.reshape(-1, logits.size(-1))  # (N*H*W x Nclasses)
+            print(mask_flat.shape, logits_flat.shape)
+            masked_logits_flat = logits_flat[mask_flat.repeat(1, nclasses)].view(
+                -1, nclasses
+            )
+
+            target_flat = target.reshape(-1, 1)  # (N*H*W x 1)
+            masked_target_flat = (
+                target_flat[mask_flat].unsqueeze(dim=-1).to(torch.int64)
+            )
+        else:
+            masked_logits_flat = logits.reshape(
+                -1, logits.size(-1)
+            )  # (N*H*W x Nclasses)
+            masked_target_flat = target.reshape(-1, 1).to(torch.int64)  # (N*H*W x 1)
+        masked_log_probs_flat = torch.nn.functional.log_softmax(
+            masked_logits_flat
+        )  # (N*H*W x Nclasses)
+        masked_losses_flat = -torch.gather(
+            masked_log_probs_flat, dim=1, index=masked_target_flat
+        )  # (N*H*W x 1)
+        if self.mean:
+            return masked_losses_flat.mean()
+        return masked_losses_flat
+
 
 def evaluate_model(model, eval_loader, criterion, device):
     """
@@ -61,8 +128,8 @@ model = TSViT(config)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 optimizer = Adam(
     model.parameters(),
-    lr=config["SOLVER"]["lr_base"],
-    weight_decay=config["SOLVER"]["weight_decay"],
+    lr=config["lr_base"],
+    weight_decay=config["weight_decay"],
 )
 
 train_loader = get_dataloader(train_csv_file, root_dir, config)
@@ -73,12 +140,16 @@ for epoch in range(epochs):
     model.train()
     total_loss = 0.0
 
-    for data, target, mask in train_loader:
-        data, target, mask = data.to(device), target.to(device), mask.to(device)
+    for samples in train_loader:
+        print(samples.keys())
+
+        data = samples["inputs"].to(device)
+        target = samples["labels"].to(device)
+        mask = samples["unk_masks"].to(device)
 
         # Forward pass
         logits = model(data)
-
+        logits = logits.permute(0, 2, 3, 1)
         # Compute loss
         loss = criterion(logits, (target, mask))
 
